a8e1e4ccb54462271f6a363c4515dc17e40b1bcc
Jack-joke <arvn1998@gmail.com>
2019-01-26 11:51:51 +0700

    New file upload

---
+++ MyApplication/app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dell.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ToA(View view) {
+    }
+
+    public void ToB(View view) {
+    }
+}
