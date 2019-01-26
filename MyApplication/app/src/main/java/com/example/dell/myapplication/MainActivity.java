bdfbba25930ef4609bf27cfafea2867c9f4b895e
lv <huongthukd@gmail.com>
2019-01-26 13:07:23 +0700

    lv _ Tinh tong

--- MyApplication/app/src/main/java/com/example/dell/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/dell/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.dell.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ToA(View view) {
+        Intent it = new Intent(MainActivity.this,TinhTongActivity.class);
+        startActivity(it);
     }
 
     public void ToB(View view) {
